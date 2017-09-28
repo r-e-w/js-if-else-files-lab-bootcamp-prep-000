7b1149f697eb6e29c746f0a8d740667ab9aa14c7
Rebecca Weiss <asw444@aol.com>
2017-09-28 00:28:26 +0000

    automatically backed up by learn

--- tests.js
+++ tests.js
@@ -5,7 +5,7 @@ console.log(index)
 if (index.name === "Susan") {
     console.log("The name is correct")
 } else {
-    console.log("Expected: Susan, Received: " index.name)
+    console.log("Expected: Susan, Received: ")
 }
 
 
